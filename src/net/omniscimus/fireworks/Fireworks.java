af3fa22895748600f3d6540bb0f6e0d0f19bfba3
Omniscimus <omniscimus@icloud.com>
2015-03-21 21:00:29 +0100

    Save shows to config and resume them on server reboot

--- src/net/omniscimus/fireworks/Fireworks.java
+++ src/net/omniscimus/fireworks/Fireworks.java
@@ -6,39 +6,51 @@ import org.bukkit.ChatColor;
 import org.bukkit.Location;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
-/**
- * @author Omniscimus
- * @version 1.2.0
- */
-
 public final class Fireworks extends JavaPlugin {
 	
-	private static ArrayList<Integer> shows;
-	private static int delay;
+	private FileConfiguration config;
 	
+	private ArrayList<Integer> shows;
+	private int delay;
+	
+	private ArrayList<Location> savedShows;
+	
+	@SuppressWarnings("unchecked")
 	@Override
 	public void onEnable() {
 		saveDefaultConfig();
+		config = getConfig();
 		shows = new ArrayList<Integer>();
-		delay = getConfig().getInt("delay");
+		delay = config.getInt("delay");
+		
+		savedShows = (ArrayList<Location>) config.getList("saved-shows");
+		if(savedShows == null || savedShows.isEmpty()) {
+			savedShows = new ArrayList<Location>();
+		}
+		
+		for(Location loc : savedShows) {
+			startShowNoSave(loc);
+		}
+		
 	}
 	
 	@Override
 	public void onDisable() {
-		stopAllShows();
-		shows.clear();
+		stopAllShowsNoSave();
 	}
 	
 	public boolean onCommand(final CommandSender sender, Command cmd, String commandLabel, String[] args) {
-		
+
 		// Return if the command is something else than /fireworks or /fw
 		if(!commandLabel.equalsIgnoreCase("fireworks") && !commandLabel.equalsIgnoreCase("fw")) return true;
 		// Return if the command is executed by the console.
 		if(!(sender instanceof Player)) {
 			sender.sendMessage(ChatColor.GOLD + "This command can only be executed by a player.");
+			return true;
 		}
 		// fw
 		if(args.length == 0) {
@@ -61,8 +73,8 @@ public final class Fireworks extends JavaPlugin {
 				sender.sendMessage(ChatColor.GOLD + "Fireworks show stopped.");
 			}
 			else if(args[0].equalsIgnoreCase("stopall")) {
-				stopAllShows();
 				sender.sendMessage(ChatColor.GOLD + "All " + numberOfShows() + " fireworks shows have stopped.");
+				stopAllShows();
 			}
 			else {
 				sender.sendMessage(ChatColor.GOLD + "Wrong command syntax. Try /fw help");
@@ -80,18 +92,35 @@ public final class Fireworks extends JavaPlugin {
 		return shows.size();
 	}
 	
-	public void startShow(Location loc) {
+	public void startShowNoSave(Location loc) {
 		shows.add(getServer().getScheduler().scheduleSyncRepeatingTask(this, new Show(loc), 0, delay));
+	}
+	public void startShow(Location loc) {
+		startShowNoSave(loc);
+		savedShows.add(loc);
+		saveShows();
 	}
 	
 	public void stopLastShow() {
 		getServer().getScheduler().cancelTask(shows.get(shows.size() - 1));
 		shows.remove(shows.size() - 1);
+		savedShows.remove(savedShows.size() - 1);
+		saveShows();
 	}
 	
-	public void stopAllShows() {
+	public void stopAllShowsNoSave() {
 		getServer().getScheduler().cancelTasks(this);
 		shows.clear();
+	}
+	public void stopAllShows() {
+		stopAllShowsNoSave();
+		savedShows.clear();
+		saveShows();
+	}
+	
+	private void saveShows() {
+		config.set("saved-shows", savedShows);
+		saveConfig();
 	}
 
 }
