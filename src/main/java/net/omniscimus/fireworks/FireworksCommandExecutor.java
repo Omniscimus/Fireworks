9717455d32930b7e68b590dfb6ae3561530bfd9d
Omniscimus <omniscimus@icloud.com>
2015-07-04 23:06:08 +0200

    Cleaned up and organised the code

---
+++ src/main/java/net/omniscimus/fireworks/FireworksCommandExecutor.java
@@ -0,0 +1,60 @@
+package net.omniscimus.fireworks;
+
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class FireworksCommandExecutor implements CommandExecutor {
+
+	private ShowHandler showHandler;
+	
+	public FireworksCommandExecutor(ShowHandler showHandler) {
+		this.showHandler = showHandler;
+	}
+	
+	@Override
+	public boolean onCommand(final CommandSender sender, Command cmd, String commandLabel, String[] args) {
+
+		// Return if the command is executed by the console.
+		if(!(sender instanceof Player)) {
+			sender.sendMessage(ChatColor.GOLD + "This command can only be executed by a player.");
+			return true;
+		}
+		// fw
+		if(args.length == 0) {
+			sender.sendMessage(ChatColor.GOLD + "-------- Fireworks --------\na plugin by Omniscimus\n" + ChatColor.RED + "Try /fw help for commands.");
+		}
+		// fw help, fw on, fw off
+		else if(args.length == 1) {
+			if(args[0].equalsIgnoreCase("?") || args[0].equalsIgnoreCase("help")) {
+				sender.sendMessage(ChatColor.GOLD + "----- Fireworks Help -----");
+				sender.sendMessage(ChatColor.GOLD + "/fw start: " + ChatColor.RED + "starts a fireworks show at your location.");
+				sender.sendMessage(ChatColor.GOLD + "/fw stop: " + ChatColor.RED + "stops the last fireworks show.");
+				sender.sendMessage(ChatColor.GOLD + "/fw stopall: " + ChatColor.RED + "stops all fireworks shows.");
+			}
+			else if(args[0].equalsIgnoreCase("start")) {
+				showHandler.startShow(((Player)sender).getLocation());
+				sender.sendMessage(ChatColor.GOLD + "Fireworks show started!");
+			}
+			else if(args[0].equalsIgnoreCase("stop")) {
+				showHandler.stopLastShow();
+				sender.sendMessage(ChatColor.GOLD + "Fireworks show stopped.");
+			}
+			else if(args[0].equalsIgnoreCase("stopall")) {
+				sender.sendMessage(ChatColor.GOLD + "All " + showHandler.getNumberOfRunningShows() + " fireworks shows have stopped.");
+				showHandler.stopAllShows();
+			}
+			else {
+				sender.sendMessage(ChatColor.GOLD + "Wrong command syntax. Try /fw help");
+			}
+		}
+		else {
+			sender.sendMessage(ChatColor.GOLD + "Wrong number of arguments. Try /fw help");
+		}
+		
+		return true;
+	}
+
+}
---
+++ src/main/java/net/omniscimus/fireworks/ShowHandler.java
@@ -0,0 +1,53 @@
+package net.omniscimus.fireworks;
+
+import java.util.ArrayList;
+
+import org.bukkit.Location;
+
+public final class ShowHandler {
+
+	private Fireworks plugin;
+	
+	private int delay;
+	private ArrayList<Integer> shows;
+	
+	public ShowHandler(Fireworks plugin, int delay) {
+		this.plugin = plugin;
+		this.delay = delay;
+		shows = new ArrayList<Integer>();
+	}
+	
+	public int getNumberOfRunningShows() {
+		return shows.size();
+	}
+	
+	// startShowNoSave is necessary so the plugin won't double-save show locations when it loads them at startup from the file.
+	public void startShowNoSave(Location loc) {
+		shows.add(plugin.getServer().getScheduler().runTaskTimer(plugin, new ShowRunnable(loc), 0, delay).getTaskId());
+	}
+	public void startShow(Location loc) {
+		startShowNoSave(loc);
+		plugin.runningShowsLocations.add(loc);
+		plugin.saveRunningShowsLocations();
+	}
+	
+	public void stopLastShow() {
+		plugin.getServer().getScheduler().cancelTask(shows.get(shows.size() - 1));
+		shows.remove(shows.size() - 1);
+		plugin.runningShowsLocations.remove(plugin.runningShowsLocations.size() - 1);
+		plugin.saveRunningShowsLocations();
+	}
+	
+	// stopAllShowsNoSave is necessary when the server closes: it doesn't erase them from the file.
+	public void stopAllShowsNoSave() {
+		plugin.getServer().getScheduler().cancelTasks(plugin);
+		shows.clear();
+	}
+	// However, when the player issues the command /fw stopall, they should be erased.
+	public void stopAllShows() {
+		stopAllShowsNoSave();
+		plugin.runningShowsLocations.clear();
+		plugin.saveRunningShowsLocations();
+	}
+	
+}
